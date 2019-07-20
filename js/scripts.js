02fb7e79256136a47b5275d7b1ba3afb0eb10869
mrsfreddy <rmogusu123@gmal.com>
2019-07-20 06:18:10 -0400

    Add about us to toggle

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+        $(".clickable").click(function() {
+          $("#info3").slideUp();
+          $("#info3 ").toggle();
+        });
+      });
+$(document).ready(function() {
+              $(".clickable1").click(function() {
+                $("#info4").slideUp();
+                $("#info4").toggle();
+              });
+            });
+  $(document).ready(function() {
+                    $(".clickable2").click(function() {
+                      $("#info5").slideUp();
+                      $("#info5").toggle();
+                    });
+                  });
