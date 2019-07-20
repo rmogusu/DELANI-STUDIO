bfcbc158589d34af1a4bcb271edb90f51b300bff
mrsfreddy <rmogusu123@gmal.com>
2019-07-20 08:45:04 -0400

    Style my portfolio in order to move

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,84 @@
 $(document).ready(function() {
-        $(".clickable").click(function() {
-          $("#info3").slideUp();
-          $("#info3 ").toggle();
-        });
-      });
+  $(".clickable").click(function() {
+    $("#info3").slideUp();
+    $("#info3").toggle();
+  });
+
+  $(".clickable1").click(function() {
+    $("#info4").slideUp();
+    $("#info4").toggle();
+  });
+
+  $(".clickable2").click(function() {
+    $("#info5").slideUp();
+    $("#info5").toggle();
+  });
+    });
+
 $(document).ready(function() {
-              $(".clickable1").click(function() {
-                $("#info4").slideUp();
-                $("#info4").toggle();
-              });
-            });
-  $(document).ready(function() {
-                    $(".clickable2").click(function() {
-                      $("#info5").slideUp();
-                      $("#info5").toggle();
-                    });
-                  });
+  $('.port1').mouseover(function() {
+    $('.text1').show();
+  }).mouseout(function() {
+    $('.text1').hide();
+  });
+
+  $('.port2').mouseover(function() {
+    $('.text2').show();
+  }).mouseout(function() {
+    $('.text2').hide();
+  });
+
+});
+
+
+$(document).ready(function() {
+
+  $('.port1').mouseover(function () {
+      $('.text1').show();
+  }).mouseout(function () {
+      $('.text1').hide();
+  });
+
+  $('.port2').mouseover(function () {
+    $('.text2').show();
+}).mouseout(function () {
+    $('.text2').hide();
+});
+
+$('.port3').mouseover(function () {
+    $('.text3').show();
+}).mouseout(function () {
+    $('.text3').hide();
+});
+
+$('.port4').mouseover(function () {
+    $('.text4').show();
+}).mouseout(function () {
+    $('.text4').hide();
+});
+
+$('.port5').mouseover(function () {
+    $('.text5').show();
+}).mouseout(function () {
+    $('.text5').hide();
+});
+$('.port6').mouseover(function () {
+    $('.text6').show();
+}).mouseout(function () {
+    $('.text6').hide();
+});
+$('.port7').mouseover(function () {
+    $('.text7').show();
+}).mouseout(function () {
+    $('.text7').hide();
+});
+$('.port8').mouseover(function () {
+    $('.text8').show();
+}).mouseout(function () {
+    $('.text8').hide();
+});
+
+
+
+
+});
